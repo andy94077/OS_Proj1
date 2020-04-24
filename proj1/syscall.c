780e241061be111bf42b2de05305a2a47b65623a
andy94077 <andy94077@gmail.com>
2020-04-24 17:05:39 +0800

    Init syscall.c and modify syscall.h

---
+++ proj1/syscall.c
@@ -0,0 +1,24 @@
+#define _GNU_SOURCE
+#include "syscall.h"
+
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/syscall.h>
+#include <time.h>
+#include <unistd.h>
+#define PRINTK 333
+#define GETNSTIMEOFDAY 334
+
+inline long printk(const char *s) {
+    return syscall(PRINTK, s);
+}
+
+inline struct timespec getnstimeofday() {
+    struct timespec ts;
+    if (syscall(GETNSTIMEOFDAY, &ts) < 0) {
+        perror("error when calling getnstimeofday");
+        exit(2);
+    }
+    return ts;
+}
--- proj1/syscall.h
+++ proj1/syscall.h
@@ -1,23 +1,10 @@
 #ifndef _INCLUDED_SYSCALL_H_
 #define _INCLUDED_SYSCALL_H_
 
-#include <errno.h>
-#include <sys/syscall.h>
 #include <time.h>
-#define PRINTK 333
-#define GETNSTIMEOFDAY 334
 
-inline long int printk(const char *s) {
-    return syscall(PRINTK, s);
-}
+inline long printk(const char *s);
 
-inline struct timespec getnstimeofday() {
-    struct timespec ts;
-    if (syscall(GETNSTIMEOFDAY, &ts) < 0) {
-        perror("error when calling getnstimeofday");
-        exit(2);
-    }
-    return ts;
-}
+inline struct timespec getnstimeofday();
 
 #endif
