c6ee088402b10fd2f1b59d5477a3f03b43809fe4
andy94077 <andy94077@gmail.com>
2020-04-27 00:06:56 +0800

    Add debug message in process.c

--- proj1/process.c
+++ proj1/process.c
@@ -12,7 +12,7 @@
 #include "syscall.h"
 
 int process_cmp(const void *a, const void *b) {
-    return ((Process *)a)->arrive_time > ((Process *)b)->arrive_time;
+    return ((Process *)a)->arrive_time - ((Process *)b)->arrive_time;
 }
 
 int process_assign_cpu(pid_t pid, unsigned int core) {
@@ -37,11 +37,11 @@ pid_t process_run(Process *proc) {
     pid_t pid = fork();
     if (pid == 0) {  // child
         process_assign_cpu(getpid(), CHILD_CPU);
-        printf("%s %d", proc->name, getpid());
+        printf("%s %d\n", proc->name, getpid());
         process_block(getpid());
         struct timespec ts_start = getnstimeofday();
 #ifdef DEBUG
-        printf("[%ld.%05ld] process %s, pid %d run\n", ts_start.tv_sec, ts_start.tv_nsec / (int)1e6, proc->name, getpid());
+        printf("[%ld.%09ld] process %s, pid %d run\n", ts_start.tv_sec, ts_start.tv_nsec / (int)1e6, proc->name, getpid());
 #endif
 
         for (int i = 0; i < proc->exec_time; i++)
@@ -49,7 +49,11 @@ pid_t process_run(Process *proc) {
 
         struct timespec ts_end = getnstimeofday();
         char msg[256];
+#ifdef DEBUG
+        sprintf(msg, "[Project1] %d %ld.%09ld %ld.%09ld %lld\n", getpid(), ts_start.tv_sec, ts_start.tv_nsec, ts_end.tv_sec, ts_end.tv_nsec, (long long)ts_end.tv_sec * (long long)1e9 + (long long)ts_end.tv_nsec - (long long)ts_start.tv_sec * (long long)1e9 - (long long)ts_start.tv_nsec);
+#else
         sprintf(msg, "[Project1] %d %ld.%09ld %ld.%09ld\n", getpid(), ts_start.tv_sec, ts_start.tv_nsec, ts_end.tv_sec, ts_end.tv_nsec);
+#endif
         printk(msg);
         exit(0);
     } else  // parent
@@ -65,7 +69,9 @@ int process_block(pid_t pid) {
 
     int ret = sched_setscheduler(pid, SCHED_IDLE, &param);
     if (ret < 0) {
-        perror("sched_setscheduler error");
+        char s[256];
+        sprintf(s, "\npid: %d block sched_setscheduler error", pid);
+        perror(s);
         exit(3);
     }
     return ret;
@@ -74,11 +80,13 @@ int process_block(pid_t pid) {
 // set high priority to the process
 int process_wakeup(pid_t pid) {
     struct sched_param param;
-    param.sched_priority = 10;
+    param.sched_priority = 9;
 
     int ret = sched_setscheduler(pid, SCHED_FIFO, &param);
     if (ret < 0) {
-        perror("sched_setscheduler error");
+        char s[256];
+        sprintf(s, "\npid: %d wakeup sched_setscheduler error", pid);
+        perror(s);
         exit(4);
     }
     return ret;
