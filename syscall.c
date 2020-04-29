eeb5c98b790ff89b46530fed68218ac998e56bba
andy94077 <andy94077@gmail.com>
2020-04-29 12:43:36 +0800

    Update syscall.c

--- syscall.c
+++ syscall.c
@@ -10,31 +10,12 @@
 #define PRINTK 333
 #define GETNSTIMEOFDAY 334
 
-/*
-long printk(const char *s) {
-    FILE *fp = fopen("/dev/kmsg", "a");
-    char buf[1024];
-    setvbuf(fp, buf, _IOLBF, sizeof(buf));
-    fprintf(fp, s);
-    fclose(fp);
-    return 0;
-}
-
-struct timespec getnstimeofday() {
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
-    return ts;
-}*/
-
 long printk(const char *s) {
     return syscall(PRINTK, s);
 }
 
 struct timespec getnstimeofday() {
     struct timespec ts;
-    if (syscall(GETNSTIMEOFDAY, &ts) < 0) {
-        perror("error when calling getnstimeofday");
-        exit(2);
-    }
+    syscall(GETNSTIMEOFDAY, &ts);
     return ts;
 }
